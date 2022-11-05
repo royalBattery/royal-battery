49f9ec011ed299a74d06c75893ec3c5db77eb7c4
KannaHashi <khaidirmuktamar234@gmail.com>
2022-11-05 22:02:49 +0800

    first change

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Royal Battery</title>
+</head>
+<body>
+    <div class="container-fluid p-0 m-0 con">
+        <nav class="navbar navbars navbar-expand nav-tr shadow-sm mt-0 px-5 justify-content-center position-fixed w-100" style="top: 0; z-index: 1;">
+            <a href="#" class="navbar-brand px-5">
+                <img src="image/logo1.jpg" class="rounded-circle" style="width: 50px; height: 50px;" alt="logo">
+            </a>
+            <div class="col">
+                <ul class="navbar-nav p-0 collapse navbar-collapse fs-5">
+                    <li class="px-4 py-3 nav-item">
+                        <a href="#" class="nav-link link-light">Home</a>
+                    </li>
+                    <li class="px-4 py-3 nav-item">
+                        <a href="#" class="nav-link link-light">Poto Kelas</a>
+                    </li>
+                    <li class="px-4 py-3 nav-item">
+                        <a href="#" class="nav-link link-light">About</a>
+                    </li>
+                    <li class="px-4 py-3 nav-item">
+                        <a href="#" class="nav-link link-light">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container-fluid p-0">
+            <div class="row align-items-center justify-content-center ulls">
+                <div class="col-5 align-items-center text-center justify-content-center">
+                    <h3 class="text-light welcomeText fw-light">welcome to</h3>
+                    <h1 class="text-light fw-bold" style="text-transform: uppercase">royal battery</h1>
+                </div>
+            </div>
+            <div class="row p-15 align-items-center">
+                <div class="col pe-5">
+                    <img src="image/writer.svg" class="float-end w-70" alt="">
+                </div>
+                <div class="col p-5 justify-content-center text-start">
+                    <h1 class="text-dark fw-bold my-4">Antonism</h1>
+                    <p class="fs-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum, elit vitae gravida finibus, quam erat varius eros, vel pellentesque orci quam in neque. Nulla eget euismod arcu, sed tincidunt libero. Pellentesque eu dui lacinia, pulvinar nulla quis, ultricies lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus sed scelerisque dui. Nulla eu purus vitae libero porttitor dictum eget quis ante.</p>
+                </div>
+            </div>
+            <div class="row p-15 align-items-center bg-purple text-light">
+                <div class="col p-5 justify-content-center text-end">
+                    <h1 class="fw-bold my-4">Antonism</h1>
+                    <p class="fs-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum, elit vitae gravida finibus, quam erat varius eros, vel pellentesque orci quam in neque. Nulla eget euismod arcu, sed tincidunt libero. Pellentesque eu dui lacinia, pulvinar nulla quis, ultricies lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus sed scelerisque dui. Nulla eu purus vitae libero porttitor dictum eget quis ante.</p>
+                </div>
+                <div class="col ps-5">
+                    <img src="image/writer.svg" class="float-start w-70" alt="">
+                </div>
+            </div>
+            <div class="row p-15 align-items-center">
+                <div class="col pe-5">
+                    <img src="image/writer.svg" class="float-end w-70" alt="">
+                </div>
+                <div class="col p-5 justify-content-center text-start">
+                    <h1 class="text-dark fw-bold my-4">Antonism</h1>
+                    <p class="fs-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum, elit vitae gravida finibus, quam erat varius eros, vel pellentesque orci quam in neque. Nulla eget euismod arcu, sed tincidunt libero. Pellentesque eu dui lacinia, pulvinar nulla quis, ultricies lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus sed scelerisque dui. Nulla eu purus vitae libero porttitor dictum eget quis ante.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
